17d6a4369d6ef4cef86d22a41458755ba8468102
AbdulKareem02 <shaik161412@gmail.com>
2025-12-18 13:29:57 +0530

    Login and Logout initial code setup

---
+++ src/components/Welcome/index.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import "./index.css";
+
+const Welcome = () => {
+  const [loginStatus, setLoginStatus] = useState(false);
+
+  return (
+    <div className="welcome-container">
+      <h1 className="heading">Hi, user Welcome to IBIX</h1>
+      {loginStatus && <button className="auth-btn">Login</button>}
+      {!loginStatus && <button className="auth-btn">Logout</button>}
+    </div>
+  );
+};
+
+export default Welcome;
